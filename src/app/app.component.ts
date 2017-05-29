f2b154e9a504a96dbb68e7f81ba1acb8fdabc3f5
Sajad Mehrabi <mehrabisajad@hotmail.com>
2017-05-29 06:04:02 +0430

    add ng2-datetimepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  dateTime = new Date();
+
+  dateTimeLtr;
+  dateTimeRtl;
+  strDateTimeLtr = '<ng2-datetimepicker [(ngModel)]="dateTimeLtr" dir="ltr"></ng2-datetimepicker>';
+  strDateTimeRtl = '<ng2-datetimepicker [(ngModel)]="dateTimeRtl" dir="rtl"></ng2-datetimepicker>';
+
+
+  dateLtr;
+  dateRtl;
+  strDateLtr = '<ng2-datetimepicker [disableTime]="true" [(ngModel)]="dateLtr" dir="ltr"></ng2-datetimepicker>';
+  strDateRtl = '<ng2-datetimepicker [disableTime]="true" [(ngModel)]="dateRtl" dir="rtl"></ng2-datetimepicker>';
+
+
+  strDateDisabledLtr = '<ng2-datetimepicker [disabled]="true" [disableTime]="true" [(ngModel)]="dateTimeLtr" dir="ltr"></ng2-datetimepicker>';
+  strDateDisabledRtl = '<ng2-datetimepicker [disabled]="true" [disableTime]="true" [(ngModel)]="dateTimeRtl" dir="rtl"></ng2-datetimepicker>';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DateTimePickerModule} from 'ng2-datetimepicker-jalali'
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DateTimePickerModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
